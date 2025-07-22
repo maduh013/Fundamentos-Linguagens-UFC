7b519099b450c53f38bb2b2113ccc538afaad09b
Maria Eduarda <163433634+maduh013@users.noreply.github.com>
2025-07-22 00:48:43 -0300

    Create exemplo_c.c

---
+++ 04 - Tipos-de-Dados/exemplo_c.c
@@ -0,0 +1,14 @@
+**Exemplos de Código**
+
+**C – Tipagem Estática e Fraca (`exemplo_c.c`)**
+
+```c
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    float y = x + 2.5; // Aceita conversão implícita
+    printf("Resultado: %f\n", y);
+    return 0;
+}
+```
