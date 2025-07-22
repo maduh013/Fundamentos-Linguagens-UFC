22d6e497e6bccb7725c8e3f64ff3bfcc2825db6e
Maria Eduarda <163433634+maduh013@users.noreply.github.com>
2025-07-22 00:55:14 -0300

    Create exemplo_js.js

---
+++ 04 - Tipos-de-Dados/exemplo_js.js
@@ -0,0 +1,10 @@
+Exemplo de Código:
+
+JavaScript – Tipagem Dinâmica e Fraca (exemplo_js.js)
+
+javascript
+Copiar
+Editar
+let x = 5;
+x = "5";
+console.log(x + 2); // Resultado: "52" (concatenação automática)
