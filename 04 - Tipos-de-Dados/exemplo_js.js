475e3ca6a0e5d2e7623dc939821572a9b936eb49
Maria Eduarda <163433634+maduh013@users.noreply.github.com>
2025-07-22 00:55:34 -0300

    Update exemplo_js.js

--- 04 - Tipos-de-Dados/exemplo_js.js
+++ 04 - Tipos-de-Dados/exemplo_js.js
@@ -2,9 +2,6 @@ Exemplo de Código:
 
 JavaScript – Tipagem Dinâmica e Fraca (exemplo_js.js)
 
-javascript
-Copiar
-Editar
 let x = 5;
 x = "5";
 console.log(x + 2); // Resultado: "52" (concatenação automática)
